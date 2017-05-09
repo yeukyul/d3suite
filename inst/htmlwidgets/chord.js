0ecb0a08e20789328f691ce54c6939649a1eb85b
Yeuk Yu Lee <yeukyul@andrew.cmu.edu>
2017-05-09 11:07:21 -0400

    fixed transform translate issue of chord chart

--- inst/htmlwidgets/chord.js
+++ inst/htmlwidgets/chord.js
@@ -12,7 +12,8 @@ HTMLWidgets.widget({
                      "#C4A29E", "#FFD07B", "#95190C", "#A63C06", "#C36F09", 
                      "#FDB833", "#710000", "#9C3848"];
 
-      var margin = 24;
+      var margin = 20,
+          radius_width = 5;
 
       var outerRadius = Math.min(width, height) / 2 - 10,
             innerRadius = outerRadius - margin;
@@ -31,21 +32,20 @@ HTMLWidgets.widget({
       var path = d3.svg.chord()
                   .radius(innerRadius);
 
-      var svg = d3.select(el).append("svg")
+
+    return {
+
+      renderValue: function(x) {
+         
+            var svg = d3.select(el).append("svg")
                   .attr("width", width)
                   .attr("height", height)
                   .append("g")
                   .attr("id", "circle")
                   .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-      svg.append("circle")
-         .attr("r", outerRadius);
-         
-      console.log("Hello from mywidget.js!");
-
-    return {
-
-      renderValue: function(x) {
+         svg.append("circle")
+            .attr("r", outerRadius);
 
             var matrix = x.mat;
             var keys = x.keys;
@@ -114,7 +114,77 @@ HTMLWidgets.widget({
 
       resize: function(width, height) {
 
-        // TODO: code to re-render the widget with a new size
+        var svg = d3.select(el).append("svg")
+                  .attr("width", width)
+                  .attr("height", height)
+                  .append("g")
+                  .attr("id", "circle")
+                  .attr("transform", "translate(-50%, 50%)");
+
+         svg.append("circle")
+            .attr("r", outerRadius);
+
+            var matrix = x.mat;
+            var keys = x.keys;
+
+            // Compute the chord layout.
+            layout.matrix(matrix);
+
+            // Add a group per neighborhood.
+            var group = svg.selectAll(".group")
+               .data(layout.groups)
+               .enter().append("g")
+               .attr("class", "group")
+               .on("mouseover", mouseover);
+
+            // Add a mouseover title.
+            group.append("title").text(function(d, i) {
+               return keys[i] + ": " + formatPercent(d.value) + " of origins";
+            });
+
+            // Add the group arc.
+            var groupPath = group.append("path")
+                              .attr("id", function(d, i) { return "group" + i; })
+                              .attr("d", arc)
+                              .style("fill", function(d, i) { return colors[i % colors.length]; });
+
+            // Add a text label.
+            var groupText = group.append("text")
+                              .attr("x", 6)
+                              .attr("dy", 15);
+
+            groupText.append("textPath")
+                     .attr("xlink:href", function(d, i) { return "#group" + i; })
+                     .text(function(d, i) { return keys[i]; });
+
+            // Remove the labels that don't fit. :(
+            groupText.filter(function(d, i) { return groupPath[0][i].getTotalLength() / 2 - 16 < this.getComputedTextLength(); })
+                     .remove();
+
+            // Add the chords.
+            var chord = svg.selectAll(".chord")
+                           .data(layout.chords)
+                           .enter().append("path")
+                           .attr("class", "chord")
+                           .style("fill", function(d) { return colors[d.source.index % colors.length]; })
+                           .attr("d", path);
+
+            // Add an elaborate mouseover title for each chord.
+             chord.append("title").text(function(d) {
+                return keys[d.source.index]
+                   + " → " + keys[d.target.index]
+                   + ": " + formatPercent(d.source.value)
+                   + "\n" + keys[d.target.index]
+                   + " → " + keys[d.source.index]
+                   + ": " + formatPercent(d.target.value);
+             });
+
+            function mouseover(d, i) {
+                  chord.classed("fade", function(p) {
+                     return p.source.index != i
+                     && p.target.index != i;
+               });
+            }
 
       }
 
